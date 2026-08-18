c8f4d3dc9e4a8f85b82cf128997807329f40c5c5
Sihle Ribisi <128409229+NotYourMom44@users.noreply.github.com>
2026-08-18 15:53:07 +0200

    Add basic building interaction system
    Implemented the basic interaction system using Unity's Input System. The player can detect nearby interactable buildings and interact with them using the E key. Added the Interactable tag to the prototype buildings.

--- Assets/Scripts/InteractionSystem.cs
+++ Assets/Scripts/InteractionSystem.cs
@@ -1,16 +1,41 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class InteractionSystem : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("Interaction Settings")]
+    [SerializeField] private float interactionRange = 3f;
+
+    private void Update()
     {
-        
+        CheckForInteraction();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void CheckForInteraction()
     {
-        
+        Collider[] colliders = Physics.OverlapSphere(
+            transform.position,
+            interactionRange
+        );
+
+        foreach (Collider collider in colliders)
+        {
+            if (collider.CompareTag("Interactable"))
+            {
+                Debug.Log("Interactable object detected: " + collider.gameObject.name);
+
+                if (Keyboard.current != null && Keyboard.current.eKey.wasPressedThisFrame)
+                {
+                    Interact(collider.gameObject);
+                }
+
+                break;
+            }
+        }
+    }
+
+    private void Interact(GameObject interactableObject)
+    {
+        Debug.Log("Interacted with: " + interactableObject.name);
     }
 }
